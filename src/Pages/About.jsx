7648dc9f9ddbdb6ad013e88fa349fe2a1baf57b1
Sanskar verma <145153887+Sanskar1431@users.noreply.github.com>
2026-06-10 16:27:43 +0530

    Update comment in About.jsx (Push 9)

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-// Weight Wearhouse Gym About Us Page
+// Weight Wearhouse Gym About Us Page (v2.1)
 import Footer from "../components/Footer/Footer";
 import GirlRunning from "../images/who-we-are/girl-run.png";
 import GirlRedBg from "../images/who-we-are/girl-redbg.svg";
