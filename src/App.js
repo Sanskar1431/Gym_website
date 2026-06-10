cd377e6c442a99db2e88b2eaee82fa3f7bc26a9a
Sanskar verma <145153887+Sanskar1431@users.noreply.github.com>
2026-06-10 16:26:28 +0530

    Update comment in App.js (Push 1)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// Weight Wearhouse Gym App Entry Point Router.
+// Weight Wearhouse Gym App Entry Point Router (v2.1).
 import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
